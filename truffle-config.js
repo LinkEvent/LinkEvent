e24147de374997e91e6ebf1d3972d154a1512470
techsavage18 <techsavage18@gmail.com>
2023-06-08 01:43:06 +0530

    add truffle config

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+module.exports = {
+  contracts_directory: "./src/contracts",
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    "kovan": {
+      network_id: 42,
+      gasPrice: 100000000000,
+      provider: new HDWalletProvider({
+        mnemonic: fs.readFileSync('./secret.env', 'utf-8'), 
+        providerOrUrl: "https://kovan.infura.io/v3/1d69cc96b8c84979933c05b7b350a175",
+        shareNonce: false,
+      })
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.10"
+    }
+  }
+};
